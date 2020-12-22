41873cf6703fe6a0c46fb79858542b4a017341cc
박진영 <nazzang49@naver.com>
2020-12-23 00:27:31 +0900

    (1223) 첫 소스 + 강의 자료 추가

---
+++ basic/grammar.py
@@ -0,0 +1,9 @@
+# import 라이브러리명
+# import pandas as pd
+
+# from 라이브러리명 import 특정 함수
+# from pandas import datetime
+
+# plain text file 호출
+data = open("../documents/00_Material(Uploaded)/00_data/text_data.txt", 'r', encoding="utf-8-sig")
+print(data.read())
