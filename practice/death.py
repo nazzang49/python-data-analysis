582901d1fe53cf9cdda912027653656088c71087
박진영 <nazzang49@naver.com>
2021-03-01 20:29:10 +0900

    사망자 수 EDA 연습

--- practice/death.py
+++ practice/death.py
@@ -1,36 +1,62 @@
 # analysis about dead people => assignment w/o any help
 
 import pandas as pd
-import numpy as np
 import os
+import json
 
 # daily reports
 dir_path = "../documents/00_Material(Uploaded)/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/"
 
-# 국가명 통일
-# 시간형식 통일
+# json file
+with open("../documents/00_Material(Uploaded)/COVID-19-master/csse_covid_19_data/country_convert.json", "r", encoding="utf-8-sig") as json_country_convert_file:
+    json_country_convert_data = json.load(json_country_convert_file)
 
+# change date format into regular
+def integrate_date_fdormat(df: pd.DataFrame, date):
+    date_format = date.split(".")[0].lstrip("0").replace("-", "/")
+    df.columns = [date_format]
+    return df
+
+# index => Country_Region
+def sum_gruop_by_country(df: pd.DataFrame):
+    return df.groupby("Country_Region").sum()
+
+# Beijing => China
+def integrate_country_name(df: pd.DataFrame):
+    if df["Country_Region"] in json_country_convert_data:
+        df["Country_Region"] = json_country_convert_data[df["Country_Region"]]
+    return df
+
+# NaN => 0 of Deaths
 def change_nan_value(df: pd.DataFrame):
     # print(df.isnull().sum(axis=0))
     # print(df[df["Deaths"].isnull()])
-    df["Deaths"] = df["Deaths"].fillna(0)
+    # df["Deaths"] = df["Deaths"].fillna(0)
+    df = df.dropna(subset=["Deaths"])
     df = df.astype({"Deaths": "int64"})
     return df
 
+# need only 3 columns
 def remove_unnecessary_column(df: pd.DataFrame):
     return df.loc[:, ["Last_Update", "Country_Region", "Deaths"]]
 
+# change column name
 def rename_column(df: pd.DataFrame):
-    if df.columns.__contains__("Country/Region"):
-        df = df.rename({"Country/Region": "Country_Region", "Last Update": "Last_Update"}, axis="columns")
+    if df.columns.__contains__("Province/State"):
+        df = df.rename({"Province/State": "Province_State", "Country/Region": "Country_Region", "Last Update": "Last_Update"}, axis="columns")
     return df
 
-def preprocessing(df: pd.DataFrame):
+# data preprocessing before visualization
+def preprocessing(df: pd.DataFrame, date):
     df = rename_column(df)
     df = remove_unnecessary_column(df)
     df = change_nan_value(df)
-    print(df.head())
+    df = df.apply(integrate_country_name, axis=1)
+    df = sum_gruop_by_country(df)
+    df = integrate_date_fdormat(df, date)
+    return df
 
+# call csv
 def return_df(path, csv_name):
     return pd.read_csv(path + csv_name, delimiter=",", encoding="utf-8-sig")
 
@@ -42,7 +68,9 @@ def generate_dateframe_by_path(path):
             csv_list.append(file_name)
 
     print(len(csv_list))
+    csv_list.sort()
 
+    first_df_flag = True
     for idx, csv_name in enumerate(csv_list):
         df = return_df(path, csv_name)
         if idx == 1:
@@ -57,11 +85,17 @@ def generate_dateframe_by_path(path):
             print(df.info())
             print(df.columns)
             print(df["Last_Update"].head())
-        preprocessing(df)
 
+        df = preprocessing(df, csv_name)
+
+        if first_df_flag:
+            final_df, first_df_flag = df, False
+        else:
+            final_df = pd.merge(final_df, df, how='outer', left_index=True, right_index=True)
+
+    final_df = final_df.fillna(0)
+    return final_df
+
+final_df = generate_dateframe_by_path(dir_path)
 generate_dateframe_by_path(dir_path)
-
-
-
-
-
+print(final_df.head())
